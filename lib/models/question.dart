02add196f3d4fafb791a643a861614e3bd4447d8
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-11-21 03:54:20 +0000

    Ajout de la classe question.dart. creation de la liste des questions

---
+++ lib/models/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String question;
+  bool reponse;
+  String explication;
+  String imagePath;
+
+  Question(this.question, this.reponse, this.explication, this.imagePath) {}
+}
--- lib/widgets/page_quizz.dart
+++ lib/widgets/page_quizz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/models/question.dart';
 import 'package:quiz/widgets/custom_text.dart';
 
 class PageQuizz extends StatefulWidget {
@@ -9,13 +10,45 @@ class PageQuizz extends StatefulWidget {
 }
 
 class _PageQuizzState extends State<PageQuizz> {
+
+  late Question question;
+
+
+  List<Question> listeQuestions = [
+    Question('La devise de la Belgique est l\'union fait la force', true, '', 'belgique.JPG'),
+    Question('La lune va finir par tomber sur terre à cause de la gravité', false, 'Au contraire la lune s\'éloigne', 'lune.jpg'),
+    Question('La Russie est plus grande en superficie que Pluton', true, '', 'russie.jpg'),
+    Question('Nyctalope est une race naine d\'antilope', false, 'C’est une aptitude à voir dans le noir', 'nyctalope.jpg'),
+    Question('Le Commodore 64 est l\’oridnateur de bureau le plus vendu', true, '', 'commodore.jpg'),
+    Question('Le nom du drapeau des pirates es black skull', false, 'Il est appelé Jolly Roger', 'pirate.png'),
+    Question('Haddock est le nom du chien Tintin', false, 'C\'est Milou', 'tintin.jpg'),
+    Question('La barbe des pharaons était fausse', true, 'A l\'époque déjà ils utilisaient des postiches', 'pharaon.jpg'),
+    Question('Au Québec tire toi une bûche veut dire viens viens t\'asseoir', true, 'La bûche, fameuse chaise de bucheron', 'buche.jpg'),
+    Question('Le module lunaire Eagle de possédait de 4Ko de Ram', true, 'Dire que je me plains avec mes 8GO de ram sur mon mac', 'eagle.jpg'),
+  ];
+  int index =0;
+  int score = 0;
+
+
   @override
+  void initState(){
+    super.initState();
+    question=listeQuestions[index];
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: CustomText("Le Quizz"),
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomText("Question numéro${index +1 }", color: Colors.grey[900],),
+          ],
+        ),
+
+      ),
     );
   }
 }
